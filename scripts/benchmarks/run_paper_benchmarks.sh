2cec8e2a28d99436e4b96634ee039d93ab4a351d
fneubuerger <neubuerger.felix@fh-swf.de>
2026-04-27 11:35:29 +0200

    feat: add support for custom seeds in benchmark script

--- scripts/benchmarks/run_paper_benchmarks.sh
+++ scripts/benchmarks/run_paper_benchmarks.sh
@@ -59,6 +59,10 @@ while [[ $# -gt 0 ]]; do
       RESUME_EXPERIMENT="$2"
       shift 2
       ;;
+    --seeds)
+      IFS=',' read -ra SEEDS <<<"$2"
+      shift 2
+      ;;
     *)
       echo "Unknown option: $1"
       exit 1
